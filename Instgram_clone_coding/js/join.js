4bd3c18a4da074bcaf77c685bfe0ad56c1810a19
yujinaa <olim920@naver.com>
2022-05-24 03:10:14 +0900

    회원가입 페이지 js 만드는중

---
+++ Instgram_clone_coding/js/join.js
@@ -0,0 +1,42 @@
+// function joinClick(){
+//     var phNum = document.getElementById("addEmail"); //getElementById는 String으로 받기 때문에 정수변환 해주기
+//     phNum = Number(phNum);
+//     var email = document.getElementById("addEmail");
+//     email = String(email);
+//     if(phNum.length==11){
+//         if(email.includes('@')){// 입력 조건이 맞다면
+//         document.getElementById("addEmail")="phNum";
+//         document.getElementById("addEmail")="email";
+//         alert('가능');
+//         }else{
+//         document.getElementById("addEmail")=null;
+//         alert("다시 입력해주세요.");
+//     } 
+// } 
+// else{
+//     alert('양식에 맞게 입력');
+// }
+// }
+function joinClick(){
+    var phNum = document.getElementById("addEmail"); //getElementById는 String으로 받기 때문에 정수변환 해주기
+    phNum = Number(phNum);
+    var email = document.getElementById("addEmail");
+    email = String(email);
+    if(phNum.length==11 && email.includes('@')){
+        document.getElementById("addEmail")="phNum";
+        document.getElementById("addEmail")="email";
+        alert('가능');
+    }else{
+        document.getElementById("addEmail")="";
+        alert("다시 입력해주세요.");
+    } 
+    alert('양식에 맞게 입력'); 
+}
+
+
+
+
+
+
+
+
