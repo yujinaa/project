4910c189776719abaf2fcfb647cfc05cf3835104
yujinaa <olim920@naver.com>
2022-05-27 01:00:29 +0900

    코드 수정

---
+++ Instgram_clone_coding/js/main.js
@@ -0,0 +1,8 @@
+const likeBtn = document.getElementsByClassName("heart");
+const likeCount = document.getElementsByClassName("like_count");
+function addLike() {
+    likeBtn.style.background = "red";
+}
+// likeBtn.addEventListener("click", function(){
+//     likeBtn.style.color = 'red';
+// });
