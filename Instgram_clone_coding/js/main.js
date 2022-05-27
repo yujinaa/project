769a5a84b3dbe35149e6e8f6dcf71696c17e8a15
yujinaa <olim920@naver.com>
2022-05-27 23:54:29 +0900

    메인페이지 댓글부분 수정

--- Instgram_clone_coding/js/main.js
+++ Instgram_clone_coding/js/main.js
@@ -1,15 +1,15 @@
-const likeBtn = document.querySelector(".heartBtn");
-// const likeCount = document.getElementsByClassName("like_count");
-function addLike(){
-    if(likeBtn.src.match("click")){
-        likeBtn.src = "img/heart.png";
-    }else{
-        likeBtn.src = "img/redheart.png";
-    }
+// const likeBtn = document.querySelector(".heartBtn");
+// // const likeCount = document.getElementsByClassName("like_count");
+// function addLike(){
+//     if(likeBtn.src.match("click")){
+//         likeBtn.src = "img/heart.png";
+//     }else{
+//         likeBtn.src = "img/redheart.png";
+//     }
 
-//         likeBtn.style.background = ''
-//     };
-likeBtn.addEventListener("click", addLike);
+// //         likeBtn.style.background = ''
+// //     };
+//likeBtn.addEventListener("click", addLike);
 
 // likeBtn.addEventListener("click", (event)=>{
 //     if(event.target.className == "xi-heart-o"){
@@ -20,20 +20,20 @@ likeBtn.addEventListener("click", addLike);
 // });
 
 
-const addReview = document.getElementsByClassName(".input_review");
-// const postReviewBtn = document.getElementsByClassName(".review_upload");
+// const addReview = document.getElementsByClassName(".input_review");
+// // const postReviewBtn = document.getElementsByClassName(".review_upload");
 
-function uploadReview(){
-    if(addReview.value){
-        let newReview = document.createElement("li");
-        newReview.innerHTML = addReview.value;
-        addReview.appendChild(newReview);
-    }else{
-        alert("댓글이 입력되지 않았습니다");
-        addReview.value = "";
+// function uploadReview(){
+//     if(addReview.value){
+//         let newReview = document.createElement("li");
+//         newReview.innerHTML = addReview.value;
+//         addReview.appendChild(newReview);
+//     }else{
+//         alert("댓글이 입력되지 않았습니다");
+//         addReview.value = "";
 
-    }
-};
+//     }
+// };
 // function uploadReview(){
 //     if(addReview.value){
 //         let newReview = document.querySelector("newComment");
@@ -50,5 +50,26 @@ function uploadReview(){
 //         newReview();
 //     }else{
 //     }
+// };
 
-// });
+const postReviewBtn = document.querySelector(".review_upload").value;
+
+addReview.addEventListener("click",()=>{
+    let addReview = document.querySelector('.input_review').value;
+    if(addReview.length !== 0){
+        postReviewBtn.classList.add('active')
+    }else{
+        postReviewBtn.classList.remove('active');
+    }
+})
+postReviewBtn.addEventListener("click",()=>{
+        uploadReview();
+})
+
+const uploadReview = () => {
+    if(postReviewBtn.value.length == 0){
+        return
+    }
+
+    
+}
