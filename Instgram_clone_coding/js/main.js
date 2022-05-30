bada935230395bbe84b35607a30015c55f0bf552
yujinaa <olim920@naver.com>
2022-05-30 15:44:19 +0900

    댓글 좋아요, 삭제 구현

--- Instgram_clone_coding/js/main.js
+++ Instgram_clone_coding/js/main.js
@@ -5,9 +5,6 @@ function addLike(){
         // pushHeartBtn.innerHTML ='<i class="xi-heart-o xi-2x"></i>';
         pushHeartBtn.style.color ='red';
 }
-// function addCancel(){
-//     if(pushHeartBtn){
-//     }
 
 // pushHeartBtn.addEventListener("click", function(){
 //     pushHeartBtn.innerHTML ='<i class="xi-heart xi-2x"></i>'
@@ -23,27 +20,55 @@ const postReviewBtn = document.querySelector(".review_upload");
 const addReview = document.querySelector('.input_review');
 const newReview = document.querySelector('.comment');
 const commenter = ['uuu', 'pdds','hooe','ddwwe'];  //db연결대신 배열로 선언
-const deleteBtn = document.querySelector('.removeComment');
-const likeBtn = document.querySelector('.likeHeart');
+// const deleteBtn = document.querySelector('.removeComment');
+// const likeBtn = document.querySelector('.likeHeart');
 
 
 //게시 버튼 클릭시 댓글 추가
 function uploadReview(){
     if(addReview.value.length > 0){
         const newComment = document.createElement("li");
+        const deleteBtn = document.createElement('removeComment');
+        const likeBtn = document.createElement('likeHeart');
         const commentervalue = Math.floor(Math.random() * commenter.length); //배열 길이만큼 무작위로 값을 생성
         const commenterPick = commenter[commentervalue];//무작위로 생성된 값을 고르도록
-        newComment.innerHTML =  commenterPick + " " + addReview.value +' ' +deleteBtn.innerHTML +likeBtn.innerHTML;
-        
+        newComment.innerHTML =  commenterPick + " " + addReview.value ;
         newReview.appendChild(newComment);
         newReview.style.fontSize ='small';
-        
+        likeBtn.innerHTML='<i class="xi-heart-o"></i>';
+        newComment.appendChild(likeBtn);
+        likeBtn.style.cursor ='pointer';
+        likeBtn.style.float='right';
+        likeBtn.style.marginLeft='-0.9rem';
+        likeBtn.style.marginRight='0.8rem';
+        likeBtn.addEventListener("click", pushHeart);
+        deleteBtn.innerHTML='<i class="xi-trash-o"></i>';
+        newComment.appendChild(deleteBtn);
+        deleteBtn.style.cursor='pointer';
+        deleteBtn.style.float='right';
+        deleteBtn.addEventListener("click", removeComm);
         addReview.value="";
         addReview.focus();
+
+        //댓글 좋아요
+        function pushHeart(){
+            if(likeBtn !== 0){
+                likeBtn.innerHTML='<i class="xi-heart"></i>';
+                likeBtn.style.color ='red';
+            }else{
+                likeBtn.innerHTML='';
+            }
+        }
+        function removeComm(){
+    
+            newComment.remove();
+        }
+
     }else{
         alert('댓글을 입력해 주세요');
     }
     postReviewBtn.style.color = "rgb(199, 235, 245)"; //댓글추가후 게시버튼 누르면 버튼색상 다시 초기화
+
 }
 
 //엔터키로 댓글 추가
@@ -63,17 +88,13 @@ function inputReview(){
         postReviewBtn.style.color = "rgb(199, 235, 245)";
     }
 }
-//댓글 좋아요 
-function pushHeart(){
-    const likeBtn = document.querySelector('.likeHeart');
-    if(likeBtn !== 0){
-        likeBtn.innerHTML='<i class="xi-heart"></i>' ;
-        likeBtn.style.color ='red';
-    }else{
-        likeBtn.innerHTML='';
-    }
-}
-//댓글 삭제
-function removeComm(){
-    newReview.remove();
-}
+
+
+// likeBtn.addEventListener("click", e=> {
+//     if(event.target.className == '<i class="xi-heart"></i>'){
+//         event.target.className = '<i class="xi-heart"></i>';
+//     } else if(event.target.className == '<i class="xi-heart-o"></i>'){
+//         event.target.className = '<i class="xi-heart-o"></i>';     
+//     }
+// });
+
